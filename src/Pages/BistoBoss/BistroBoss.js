8c7eb8b4b7ddc5004b09aeff357a13d500675d22
hasantalukder1234 <hasantalukdercou@gmail.com>
2023-08-30 15:23:31 +0600

    Menu Section Added & desgin React Parallax

--- src/Pages/BistoBoss/BistroBoss.js
+++ src/Pages/BistoBoss/BistroBoss.js
@@ -3,17 +3,17 @@ import './Bisto.css';
 
 const BistroBoss = () => {
     return (
-        <div className='bg-desgin'>
-            <div className='text-center w-70%'>
-              
-                <h3 className='text-4xl text-black mx-20'>Bistro Boss</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                     Necessitatibus, libero accusamus laborum deserunt ratione 
-                     dolor officiis praesentium! Deserunt magni aperiam dolor 
-                     eius dolore at, nihil iusto ducimus incidunt quibusdam nemo.</p>
-                     </div>
+        <div className="hero min-h-screen" style={{ backgroundImage: 'url(../../../../assets/home/chef-service.jpg)' }}>
+            <div className="hero-overlay bg-opacity-60"></div>
+            <div className="hero-content text-center text-neutral-content">
+                <div className="max-w-md">
+                    <h1 className="mb-5 text-5xl font-bold">Bistro Boss</h1>
+                    <p className="mb-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, libero accusamus laborum deserunt ratione dolor officiis praesentium! Deserunt magni aperiam dolor eius dolore at, nihil iusto ducimus incidunt quibusdam nemo.</p>
+                        
+                </div>
             </div>
-        
+        </div>
+
     );
 };
 
--- src/Pages/Menu/Menu/Menu.js
+++ src/Pages/Menu/Menu/Menu.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
+import Cover from '../../Shared/Cover/Cover';
+import menuImg from '../../../assets/home/chef-service.jpg';
+import PopularMenu from '../../Home/PopularMenu/PopularMenu';
 
 const Menu = () => {
     return (
         <div>
-                <Helmet>
-        <title>Bistro Boss | Menu</title>
-      
-      </Helmet>
-            <h2>Our Menu Items</h2>
+            <Helmet>
+                <title>Bistro Boss | Menu</title>
+
+            </Helmet>
+            <Cover img={menuImg}
+            title="Our Menu"
+            details ="Would you like to try a Dish."></Cover>
+           <PopularMenu></PopularMenu>
+           <Cover img={menuImg}
+            title="Our Menu"
+            details ="Would you like to try a Dish."></Cover>
+           <PopularMenu></PopularMenu>
+
         </div>
     );
 };
