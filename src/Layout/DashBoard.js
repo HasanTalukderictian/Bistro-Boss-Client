dc81f14a3c64a511858d6631b829f7ec9b517a6c
hasantalukder1234 <hasantalukdercou@gmail.com>
2023-09-04 11:05:06 +0600

    Cart Page Desgin and Delete Data from Cart

--- src/Layout/DashBoard.js
+++ src/Layout/DashBoard.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link, NavLink, Outlet } from 'react-router-dom';
 import { FaShoppingCart, FaWallet, FaCalendar, FaHome } from 'react-icons/fa';
+import useCart from '../hooks/useCart';
 // import '../App.css';
 
 const DashBoard = () => {
-
-
+    
+    const [cart] = useCart();
+    
     return (
         <div className="drawer lg:drawer-open ">
             <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
@@ -21,8 +23,14 @@ const DashBoard = () => {
                     <li><NavLink to='/dashboard/reservations'><FaCalendar></FaCalendar>Calendar</NavLink></li>
 
                     <li><NavLink to='/dashboard/history'><FaWallet></FaWallet>Payment History</NavLink></li>
-                    <li><NavLink to="/dashboard/mycart"><FaShoppingCart></FaShoppingCart>My Cart</NavLink></li>
+
+                    <li><NavLink to="/dashboard/mycart"><FaShoppingCart></FaShoppingCart>My Cart
+                    <span className="badge inline badge-secondary">{cart?.length || 0}</span>
+                    </NavLink></li>
+                   
                     <div className="divider"></div>
+
+
                     <li><NavLink to='/'><FaHome></FaHome>Home</NavLink></li>
                     <li><NavLink to='/menu'>Our Menu</NavLink></li>
                     <li><NavLink to='/order/salad'>Order</NavLink></li>
--- src/Pages/DashBoard/MyCart.js
+++ src/Pages/DashBoard/MyCart.js
@@ -1,23 +1,102 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
 import useCart from '../../hooks/useCart';
+import { FaTrash } from 'react-icons/fa';
+import Swal from 'sweetalert2';
 
 const MyCart = () => {
-    const [cart] =useCart();
+    const [cart, refetch] = useCart();
     console.log(cart);
+
+    // How Does reduce work!!!!
     const total = cart.reduce((sum, item) => item.price + sum, 0)
+
+    const handleDelete = item =>{
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+          }).then((result) => {
+            if (result.isConfirmed) {
+              fetch(`http://localhost:5000/carts/${item._id}`, {
+                method:'DELETE',
+
+              })
+              .then(res => res.json())
+              .then( data => {
+                if(data.deleteCount > 0){
+                    fetch();
+                    Swal.fire(
+                        'Deleted!',
+                        'Your file has been deleted.',
+                        'success'
+                      )
+                }
+              })
+            }
+          })
+    }
 
     return (
 
-        <div>
-             <Helmet>
+        <div className='w-full'>
+            <Helmet>
                 <title>Bistro Boss | My Cart</title>
             </Helmet>
 
-            <div className='uppercase'>
-                  <h3 className="text-3xl"> Total items: {cart.length}</h3>
-                  <h3 className="text-3xl"> Total price: ${total}</h3>
-                  <button className="btn btn-warning btn-sm">Pay</button>
+            <div className='uppercase font-semibold flex justify-evenly h-10 items-center'>
+                <h3 className="text-3xl"> Total items: {cart.length}</h3>
+                <h3 className="text-3xl"> Total price: ${total}</h3>
+                <button className="btn btn-warning btn-sm">Pay</button>
+            </div>
+            <div className="overflow-x-auto">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Food</th>
+                            <th>Item Name</th>
+                            <th>Price</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            cart.map((item, index) => <tr
+                                key={item._id}>
+                                <td>
+                                    {index + 1}
+                                </td>
+                                <td>
+
+                                    <div className="avatar">
+                                        <div className="mask mask-squircle w-12 h-12">
+                                            <img src={item.image} alt="Avatar Tailwind CSS Component" />
+                                        </div>
+                                    </div>
+
+
+                                </td>
+                                <td>
+                                {item.name}
+                                </td>
+                                <td className='text-end'>${item.price}</td>
+                                <td>
+                                    <button onClick={()=>handleDelete(item)} className="btn btn-ghost btn-lg bg-red-400 text-white"><FaTrash></FaTrash></button>
+                                </td>
+                            </tr>)
+                        }
+
+
+                    </tbody>
+
+
+                </table>
             </div>
         </div>
     );
