af24528c6568556c2a0ab1db1ff5302f92887965
hasantalukder1234 <hasantalukdercou@gmail.com>
2023-09-06 08:57:45 +0600

    Create  JWT Token

--- src/Providers/AuthProvider.js
+++ src/Providers/AuthProvider.js
@@ -1,6 +1,7 @@
 import React, { createContext, useEffect, useState } from 'react';
 import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import { app } from '../firebase/firebase.config';
+import axios from 'axios';
 
 
 
@@ -44,8 +45,28 @@ const AuthProvider = ({children}) => {
 
     useEffect(()=>{
         const unsubscribe = onAuthStateChanged(auth, currentUser =>{
-            setUser(currentUser);
-            console.log('current user', currentUser);
+            
+            // console.log('current user', currentUser);
+             setUser(currentUser);
+            // set and get token 
+            
+     
+
+           if(currentUser){
+            axios.post('http://localhost:5000/jwt', {email: currentUser.email})
+            .then(data =>{
+                // console.log(data.data.token)
+                localStorage.setItem('access-token', data.data.token)
+                setLoading(false);
+            })
+            .catch(error =>{
+                console.log(error);
+            })
+        }
+        else{
+            localStorage.removeItem('access-token')
+        }
+
             setLoading(false);
         });
         return () => {
