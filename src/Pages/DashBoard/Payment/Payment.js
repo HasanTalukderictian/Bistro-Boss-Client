a48ece9bb9a5be83f5a74a8394f8338f47ece625
hasantalukder1234 <hasantalukdercou@gmail.com>
2023-09-11 11:40:55 +0600

    create stripe account and use publishble key for payment

--- src/Pages/DashBoard/Payment/Payment.js
+++ src/Pages/DashBoard/Payment/Payment.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import SectionTitle from '../../../Components/SectionTitle/SectionTitle';
+import CheckOutForm from './CheckOutForm';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+
+
+
+const stripePromise = loadStripe('process.env.REACT_APP_PAYMENT_KEY');
 
 const Payment = () => {
     return (
         <div className='w-full'>
             <SectionTitle Subheading="please proceed to Payment" heading="Payment"></SectionTitle>
             <h2 className='text-3xl'>Payment Here</h2>
+            <Elements stripe={stripePromise}>
+            <CheckOutForm></CheckOutForm>
+            </Elements>
+            
         </div>
     );
 };
